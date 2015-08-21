b796ff2619815e2174620e03f5133d7d3497c932
Michael J. Ryan <tracker1@gmail.com>
2015-08-21 09:58:27 -0700

    random integer generator working.

--- src/util/random.js
+++ src/util/random.js
@@ -1,11 +1,17 @@
+import crypto from 'crypto';
 
 export default function getRandom(a=0, b=0) {
   //force integer values.
   a = ~~a;
   b = ~~b;
   
+  if (a < 0) throw new Error('Values must be positive numbers.')
   if (a == b) return a; //same value
-  if (b < a) [a,b] = [b,a]; //lower-higher
+  if (b < a) [a,b] = [b,a]; //lower-higher  
   
-  
+  var random = parseInt(crypto.randomBytes(4).toString('hex'), 16);
+  if (random < 0) random = random * -1; //make it positive if negative value
+   
+  //get random value against range
+  return (random % (b - a + 1)) + a;
 }
